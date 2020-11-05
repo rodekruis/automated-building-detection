1746431abb6dca263a752d656dbddcf3bfc77977
jmargutti <jacopo.margutti@gmail.com>
2020-11-05 16:33:32 +0100

    fix directory structure

--- abd_utils/src/abd_utils/images_to_abd.py
+++ abd_utils/src/abd_utils/images_to_abd.py
@@ -20,18 +20,18 @@ def main(images, output):
     for file in tqdm(list_tiles):
         # create new directory
         zoom, x, y, ext = file.split('.')
-        os.makedirs(os.path.join(output, zoom), exist_ok=True)
-        os.makedirs(os.path.join(output, zoom, x), exist_ok=True)
+        os.makedirs(os.path.join(output, 'images', zoom), exist_ok=True)
+        os.makedirs(os.path.join(output, 'images', zoom, x), exist_ok=True)
         # save as tiff
         im = Image.open(os.path.join(images, file))
         im_resized = im.resize((512, 512))
-        im_resized.save(os.path.join(output, zoom, x, y + '.tiff'))
+        im_resized.save(os.path.join(output, 'images', zoom, x, y + '.tiff'))
         # add to cover
         cover = cover.append(pd.Series({'x': x,
                                         'y': y,
                                         'z': zoom}), ignore_index=True)
     # save cover
-    cover.to_csv(os.path.join(output, 'images', 'cover.csv'), header=False, index=False)
+    cover.to_csv(os.path.join(output, 'cover.csv'), header=False, index=False)
 
 
 if __name__ == '__main__':
