ba4ccb3ea3e8aa720dec55a6fc0726e219081aaf
Wessel93 <wes_dejong9@hotmail.com>
2021-10-04 11:51:03 +0200

    Return 1 when no buildings on image

--- abd_model/src/abd_model/metrics/IoU.py
+++ abd_model/src/abd_model/metrics/IoU.py
@@ -5,12 +5,12 @@ def get(label, predicted, config=None):
 
     tn, fn, fp, tp = confusion(label, predicted)
     if tp == 0 and fp == 0 and fn == 0:
-        return float("NaN")
+        return 1
 
     try:
         assert tp or fp or fn
         iou = float(tp / (fp + fn + tp))
     except ZeroDivisionError:
-        iou = float("NaN")
+        iou = 1
 
     return iou
